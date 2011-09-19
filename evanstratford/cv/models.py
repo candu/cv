68349442787ee22ceea3a60a5dd7e6f91715bd09
Fearless Tost <me@fearlesstost.com>
2011-09-18 20:39:05 -0700

    Adding models

--- evanstratford/cv/models.py
+++ evanstratford/cv/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Tag(models.Model):
-  path = models.CharField(255)
-  description = models.CharField(255)
+  path = models.CharField(max_length=255)
+  description = models.CharField(max_length=255)
 
 class Activity(models.Model):
   tags = models.ManyToManyField(Tag)
   sub_activities = models.ForeignKey('self')
-  name = models.CharField(255)
-  description = models.CharField(255)
+  name = models.CharField(max_length=255)
+  description = models.CharField(max_length=255)
   started = models.DateTimeField()
   finished = models.DateTimeField()
