d5d1783b667158d3e2d8f31b394c5da81678fbf7
Evan Savage <savage.evan@gmail.com>
2012-10-18 15:05:35 -0700

    Sitemap for CV
    This turned out to be way more complicated than expected for a simple
    one-page static sitemap. Grrr.

--- django_root/urls.py
+++ django_root/urls.py
@@ -1,5 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.sitemaps import Sitemap
+
+class StaticSitemap(Sitemap):
+  priority = 1.0
+  lastmod = None
+
+  def items(self):
+    return ['/']
+
+  def location(self, obj):
+    return obj
+
+sitemaps = {
+  'static': StaticSitemap
+}
 
 urlpatterns = patterns('',
-    url(r'^(.*)$', 'cv.views.index'),
+  (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+  url(r'^(.*)$', 'cv.views.index')
 )
